690c27b3b0621cf84449de00ceb8e95de6bbdec7
priyanka2645 <108840209+priyanka2645@users.noreply.github.com>
2025-11-02 05:59:30 +0530

    Add files via upload

---
+++ program1.1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int Divided(int ino1, int ino2)
+{
+    int ians = 0;
+    if(ino2<=2)
+    {
+        return -1;
+
+    }
+
+    ians = ino1/ ino2;
+    return inas;
+
+}
+
+
+
+int main()
+{
+    int iVlaue1 = 15, iVlaue2 = 5;
+
+    int iRet = 0;
+
+
+    iRet Divided( iVlaue1, iVlaue2);
+
+
+    printf(" Division is %d", &iRet);
+
+
+
+return 0;
+}
+
